80f23035b9f511211225ce7bb466db154bccf966
Robby Jay Navarro <nrobbyjay@github.com>
2025-07-17 16:20:35 +0000

    Set database environment

--- api_src/app.js
+++ api_src/app.js
@@ -1,9 +1,17 @@
 const express = require("express")
-
 const app = express()
+app.use(express.json())
 
-app.get("/", (req, res)=>{
-    res.send('testing')
+app.post("/create/order/delivery", (req, res)=>{
+    const {name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverDateTime} = req.body
+    console.log('/create/order/pickup:' + name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverAddress, deliverDateTime)
+    res.sendStatus(200)
+})
+
+app.post("/create/order/pickup", (req, res)=>{
+    const {name, email, contactNumber, includeDetergent, includeFabcon, pickupDateTime} = req.body
+    console.log('/create/order/pickup:' + name, email, contactNumber, includeDetergent, includeFabcon, pickupDateTime)
+    res.sendStatus(200)
 })
 
 app.listen(3000, '0.0.0.0', ()=>{
