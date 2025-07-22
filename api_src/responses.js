608a211addbfc1e5436263d63d07354830e505b4
Robby Jay Navarro <>
2025-07-22 17:16:13 +0000

    Refactor code, added responses.js to control api response later. Pickup order creation has been finished and endpoint is now ready to accept request

---
+++ api_src/responses.js
@@ -0,0 +1,3 @@
+export const FailReturns = (err)=>{
+    return err
+}
--- api_src/routes.js
+++ api_src/routes.js
@@ -1,53 +1,46 @@
 import {Router} from 'express'
 
-import {con} from './db.js'
+import {getDeliveries, getPickups, newDelivery, newPickup} from './db.js'
+import {FailReturns} from './responses.js'
 
 export const router = Router() 
 
 router.post("/create/order/delivery", async (req, res) => {
     const { name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverDateTime } = req.body
-    const query = `
-        INSERT INTO orders(name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverDateTime, type)
-         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
-        `
-    try {
-        let db = con()
-        await db.execute(query, [name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverDateTime, "delivery"])
+    try{
+        newDelivery(name, email, contactNumber, includeDetergent, includeFabcon, deliverAddress, pickupDateTime, deliverDateTime)
         res.sendStatus(200)
-    }
-    catch (err) {
-        console.log(err)
-        res.sendStatus(500)
+    }catch(err){
+        FailReturns(err)
     }
 
 })
 
-//undone
 router.post("/create/order/pickup", (req, res) => {
     const { name, email, contactNumber, includeDetergent, includeFabcon, pickupDateTime } = req.body
-    console.log('/create/order/pickup: ' + name, email, contactNumber, includeDetergent, includeFabcon, pickupDateTime)
-    res.sendStatus(200)
+    try{
+        newPickup(name, email, contactNumber, includeDetergent, includeFabcon, pickupDateTime)
+        res.sendStatus(200)
+    }catch(err){
+        FailReturns(err)
+    }
 
 })
 
-router.get("/orders/delivery", (req, res) => {
-    let db = con()
-    db.query("SELECT * FROM orders WHERE type = ?", ["delivery"], (err, results) => {
-        if (err) {
-            res.sendStatus(500)
-            console.log(err)
-        }
-        res.json(results)
-    })
+router.get("/orders/delivery", async (req, res) => {
+     try{
+        const sd = await getDeliveries()
+        res.json(await sd[0])
+    }catch(err){
+        res.send(FailReturns(err))
+    }
 })
 
-router.get("/orders/pickup", (req, res) => {
-    let db = con()
-    db.query("SELECT * FROM orders WHERE type = ?", ["pickup"], (err, results) => {
-        if (err) {
-            res.sendStatus(500)
-            console.log(err)
-        }
-        res.json(results)
-    })
+router.get("/orders/pickup", async (req, res) => {
+     try{
+        const sd = await getPickups()
+        res.json(await sd[0])
+    }catch(err){
+        res.send(FailReturns(err))
+    }
 })
